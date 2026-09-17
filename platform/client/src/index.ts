98001fd6f63aceff29002dc1442f35cefdf397f3
Mike Cheng <minyuc2@illinois.edu>
2026-09-16 22:19:41 -0500

    feat(platform): add the shared i18n engine

--- platform/client/src/index.ts
+++ platform/client/src/index.ts
@@ -1,2 +1,4 @@
 export { assertDictParity, dictParityIssues } from "./testing";
 export type { ParityOptions } from "./testing";
+export { createI18n, resolveLang } from "./i18n";
+export type { I18n, I18nOptions, Lang } from "./i18n";
